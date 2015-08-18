f1e44a91cc8ebaa54aadf56a1e51b81a42c628d3
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-08-18 23:00:36 +0900

    Complete Lorenz sample

--- app/Lorenz.hs
+++ app/Lorenz.hs
@@ -1,25 +1,43 @@
 
-import Odeint()
+import Odeint
+import Text.Printf
+import Prelude as P
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Interpreter as I
 
 type V = Vector Double
 
-stack :: [Exp Double] -> Acc V
-stack (x:xs) = (A.++) v $ stack xs
+lorenz :: Acc V -> Acc V -> Acc V
+lorenz mu v = stack [p*(y-x), x*(r-z)-y, x*y - b*z]
   where
-    v = fill (index1 1) x :: Acc V
-stack [] = fill (index1 0) 0
-
-lorenz :: Acc V -> Acc V
-lorenz v = stack [y, x, z]
-  where
+    p = mu ! (index1 0)
+    r = mu ! (index1 1)
+    b = mu ! (index1 2)
     x = v ! (index1 0)
     y = v ! (index1 1)
     z = v ! (index1 2)
 
+timeline :: (Acc V -> Acc V) -> Acc V -> [Acc V]
+timeline f v = vn:(timeline f vn)
+  where vn = use $ run $ f v
+
+printToCSV :: [Acc V] -> IO()
+printToCSV (t:tl) = do
+  let (x:y:z:_) = toList $ run t
+  printf "%f,%f,%f\n" x y z
+  printToCSV tl
+printToCSV [] = return ()
+
+slice' :: Int -> [a] -> [a]
+slice' i (x:xs) = x:(slice' i $ P.drop (i-1) xs)
+slice' _ [] = []
+
 main :: IO ()
 main = do
-  let arr = use $ fromList (Z:.3) [1..] :: Acc V
-  print $ lorenz arr
-  print $ run $ lorenz arr
+  let arr = use $ fromList (Z:.3) [1, 0, 0] :: Acc V
+  let mu = use $ fromList (Z:.3) [10, 28, 8.0/3.0] :: Acc V
+  let dt = 0.01 :: Exp Double
+  let f = eEuler (lorenz mu) dt
+  let tl = timeline f arr :: [Acc V]
+  putStrLn "x,y,z"
+  printToCSV $ slice' 10 $ P.take 10000 tl
--- src/Odeint.hs
+++ src/Odeint.hs
@@ -3,31 +3,32 @@
 module Odeint
     ( eEuler
     , rEuler
+    , stack
     ) where
 
 import Data.Array.Accelerate as A
 
-type DVector = Vector Double
+type V = Vector Double
 
-plus :: Acc DVector -> Acc DVector -> Acc DVector
+plus :: Acc V -> Acc V -> Acc V
 plus x y = A.zipWith (+) x y
 
-mult :: Exp Double -> Acc DVector -> Acc DVector
+mult :: Exp Double -> Acc V -> Acc V
 mult a x = A.map (*a) x
 
-eEuler :: (Acc DVector -> Acc DVector)
-       -> Exp Double
-       -> Acc DVector
-       -> Acc DVector
+eEuler :: (Acc V -> Acc V) -> Exp Double -> Acc V -> Acc V
 eEuler f dt !vec = vec `plus` dx
   where
     dx = dt `mult` f vec
 
-rEuler :: (Acc DVector -> Acc DVector)
-       -> Exp Double
-       -> Acc DVector
-       -> Acc DVector
+rEuler :: (Acc V -> Acc V) -> Exp Double -> Acc V -> Acc V
 rEuler f dt !vec = vec `plus` k1 `plus` k2
   where
     k1 = (dt / 2) `mult` f vec
     k2 = (dt / 2) `mult` f (vec `plus` k1)
+
+stack :: [Exp Double] -> Acc V
+stack (x:xs) = (A.++) v $ stack xs
+  where
+    v = fill (index1 1) x :: Acc V
+stack [] = fill (index1 0) 0
