2dfe5bda192d72a00312392de9dc05a9d8e8d928
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-08-27 15:28:05 +0900

    Revise demo (lorenz)

--- app/Lorenz.hs
+++ app/Lorenz.hs
@@ -1,6 +1,7 @@
 
 import Odeint
 import Numeric.LinearAlgebra
+import Text.Printf
 
 type V = Vector Double
 
@@ -14,9 +15,22 @@ lorenz mu v = vector [p*(y-x), x*(r-z)-y, x*y - b*z]
     y = v ! 1
     z = v ! 2
 
+timeline :: (V -> V) -> V -> [V]
+timeline teo x0 = x1:timeline teo x1
+  where
+    x1 = teo x0
+
+printCSV :: [V] -> IO ()
+printCSV (v:vs) = printf "%f %f %f\n" x y z >> printCSV vs
+  where
+    x = v ! 0
+    y = v ! 1
+    z = v ! 2
+printCSV [] = return ()
+
 main :: IO ()
 main = do
   let mu = vector [10, 28, 8.0/3.0]
-  let teo = eEuler (lorenz mu) 0.01
+  let teo = rk4 (lorenz mu) 0.01
   let v = vector [1, 0, 0]
-  print $ teo v
+  printCSV $ take 10000 $ timeline teo v
