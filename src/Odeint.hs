069f3a6841c49fa37ea726d813af2241551781e7
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-10 00:43:59 +0900

    Write rEuler

--- src/Odeint.hs
+++ src/Odeint.hs
@@ -2,15 +2,28 @@
 
 module Odeint
     ( eEuler
+    , rEuler
     ) where
 
 import Data.Array.Repa as Repa
 
-eEuler :: (Num a, Shape sh, Source r1 a, Source r2 a)
-       => (Array r1 sh a -> Array r2 sh a)
-       -> a
-       -> Array r1 sh a
-       -> Array D sh a
-eEuler f dt vec = Repa.zipWith (\ v x -> v + dt * x) vec dx
+
+xpy p = Repa.zipWith (\ x y -> x + p*y)
+
+eEuler :: (Shape sh)
+       => (Array U sh Double -> Array U sh Double)
+       -> Double
+       -> Array U sh Double
+       -> Array U sh Double
+eEuler f dt vec = computeS $ (xpy dt) vec (f vec)
+
+rEuler :: (Shape sh)
+       => (Array U sh Double -> Array U sh Double)
+       -> Double
+       -> Array U sh Double
+       -> Array U sh Double
+rEuler f dt vec = computeS $ Repa.zipWith (+) l1 k2
   where
-    !dx = f vec
+    k1 = f vec
+    l1 = computeS $ (xpy dt) vec k1
+    k2 = f l1
