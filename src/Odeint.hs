3e83805dfb0b6316deb54881d4980938e5d633d2
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-10 01:25:13 +0900

    Impl RK4

--- src/Odeint.hs
+++ src/Odeint.hs
@@ -3,6 +3,7 @@
 module Odeint
     ( eEuler
     , rEuler
+    , rk4
     ) where
 
 import Data.Array.Repa as Repa
@@ -27,3 +28,18 @@ rEuler f dt vec = computeS $ Repa.zipWith (+) l1 k2
     k1 = f vec
     l1 = computeS $ (xpy dt) vec k1
     k2 = f l1
+
+rk4 :: (Shape sh)
+    => (Array U sh Double -> Array U sh Double)
+    -> Double
+    -> Array U sh Double
+    -> Array U sh Double
+rk4 f dt vec = computeS $ (xpy $ dt/6.0) vec $ (xpy 2.0) (k1 +^ k4) (k2 +^ k3)
+  where
+    k1 = f vec
+    l1 = computeS $ (xpy $ 0.5*dt) vec k1
+    k2 = f l1
+    l2 = computeS $ (xpy $ 0.5*dt) vec k2
+    k3 = f l2
+    l3 = computeS $ (xpy dt) vec k3
+    k4 = f l3
